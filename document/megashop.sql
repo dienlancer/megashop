2f3b8fe4beefa5f4e3ce10f3d768303764ae86f3
NguyenKimDien <dienit02@gmail.com>
2017-11-26 23:58:23 +0700

    cap nhat

--- document/megashop.sql
+++ document/megashop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 26, 2017 lúc 05:56 PM
+-- Thời gian đã tạo: Th10 26, 2017 lúc 05:57 PM
 -- Phiên bản máy phục vụ: 10.1.22-MariaDB
 -- Phiên bản PHP: 7.1.4
 
